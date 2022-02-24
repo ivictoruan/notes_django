bb6fc8b96fdba03f8da66574b69e68aaeb71fdff
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-02-24 18:14:16 -0300

    add secret key

--- notes/settings.py
+++ notes/settings.py
@@ -11,7 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-cfd%uu^-8o^b281^qq&*b*_@m^+k^7m*d&d2ryu^#$k&g$p$8("
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
